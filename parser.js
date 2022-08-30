e4e0d8d5c275f5488407c1f3832389a9f53b4437
Harry Zhou <harryzhou.swe@gmail.com>
2022-08-30 08:05:35 -0700

    Added more parser logic

--- parser.js
+++ parser.js
@@ -1,13 +1,54 @@
 "use strict";
 exports.__esModule = true;
 var fs_1 = require("fs");
+var omitConfigComments = function (fileArray) {
+    return fileArray.filter(function (line) { return !line.includes('#'); });
+};
 var readConfigFile = function (file) {
-    if (!file) {
-        return null;
-    }
     var fileContents = (0, fs_1.readFileSync)(file, 'utf-8');
     var fileArray = fileContents.split(/\r?\n/);
-    console.log(fileArray);
     return fileArray;
 };
-readConfigFile('./config.txt');
+var generateKeyValues = function (fileArray) {
+    return fileArray.map(function (item) {
+        return item.split('=').map(function (value) { return value.trim(); });
+    });
+};
+var generateConfigObject = function (fileArray) {
+    return Object.fromEntries(fileArray);
+};
+var isNumeric = function (num) {
+    return (typeof num === 'number' ||
+        (typeof num === 'string' && num.trim() !== '')) &&
+        !isNaN(num);
+};
+var convertToNumbers = function (object) {
+    Object.keys(object).forEach(function (key) {
+        if (isNumeric(object[key])) {
+            object[key] = parseFloat(object[key]);
+        }
+    });
+};
+var convertToBool = function (object) {
+    var boolOptions = {
+        yes: true,
+        no: false,
+        on: true,
+        off: false,
+        "true": true,
+        "false": false
+    };
+    Object.keys(object).forEach(function (key) {
+        if (object[key] in boolOptions) {
+            object[key] = boolOptions[object[key]];
+        }
+    });
+};
+var parseConfigTextFile = function (fileLocation) {
+    var stringObject = generateConfigObject(generateKeyValues(omitConfigComments(readConfigFile(fileLocation))));
+    convertToNumbers(stringObject);
+    convertToBool(stringObject);
+    return stringObject;
+};
+var configurationObject = parseConfigTextFile('./config.txt');
+console.log(configurationObject.test_mode);
--- parser.ts
+++ parser.ts
@@ -1,13 +1,67 @@
-import { readFileSync, promises as fsPromises } from 'fs';
+import { readFileSync } from 'fs';
 
-const readConfigFile = (file: string): string[] | null => {
-    if (!file) {
-        return null;
-    }
+type ConfigObject = {
+    [key: string]: any;
+};
+
+const omitConfigComments = (fileArray: string[]): string[] => {
+    return fileArray.filter((line) => !line.includes('#'));
+};
+
+const readConfigFile = (file: string): string[] => {
     const fileContents = readFileSync(file, 'utf-8');
     const fileArray = fileContents.split(/\r?\n/);
-    console.log(fileArray);
     return fileArray;
 };
 
-readConfigFile('./config.txt');
+const generateKeyValues = (fileArray: string[]): any => {
+    return fileArray.map((item: string) =>
+        item.split('=').map((value: string) => value.trim())
+    );
+};
+
+const generateConfigObject = (fileArray: any[]): ConfigObject => {
+    return Object.fromEntries(fileArray);
+};
+
+const isNumeric = (num: any) =>
+    (typeof num === 'number' ||
+        (typeof num === 'string' && num.trim() !== '')) &&
+    !isNaN(num as number);
+
+const convertToNumbers = (object: ConfigObject): void => {
+    Object.keys(object).forEach((key) => {
+        if (isNumeric(object[key])) {
+            object[key] = parseFloat(object[key]);
+        }
+    });
+};
+
+const convertToBool = (object: ConfigObject): void => {
+    const boolOptions = {
+        yes: true,
+        no: false,
+        on: true,
+        off: false,
+        true: true,
+        false: false,
+    };
+
+    Object.keys(object).forEach((key) => {
+        if (object[key] in boolOptions) {
+            object[key] = boolOptions[object[key]];
+        }
+    });
+};
+
+const parseConfigTextFile = (fileLocation: string): ConfigObject => {
+    const stringObject = generateConfigObject(
+        generateKeyValues(omitConfigComments(readConfigFile(fileLocation)))
+    );
+    convertToNumbers(stringObject);
+    convertToBool(stringObject);
+    return stringObject;
+};
+
+const configurationObject = parseConfigTextFile('./config.txt');
+console.log(configurationObject);
